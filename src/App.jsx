2cfef8f4a040d712efa15f9337249e3da36329b6
SLU Kompetenscentrum företagsledning <emil.akesson@slu.se>
2025-05-28 12:07:14 +0200

    Auto publish

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
         )}
         {view === "advice" && <AdviceView onBack={() => setView("loop")} />}
         <div className="absolute bottom-2 right-2 text-xs text-white/60 z-20">
-          v1.0.1
+          v1.0.2
         </div>
       </div>
     </div>
